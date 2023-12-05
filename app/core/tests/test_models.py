48ed95949897df5c1c2bd37bb7f6d896646311a3
daveoo-k <d.kociubinski@gmail.com>
2023-12-05 09:54:37 +0100

    Starting Recipie App

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,12 @@
 """
 Test for models.
 """
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -45,3 +49,19 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipie(self):
+        """Test createing a recipie is successfull"""
+        user = get_user_model().objects.create_user(
+                'test@exampl.com',
+                'test12345',
+        )
+        recipie = models.Recipie.objects.create(
+            user=user,
+            title='Sample recipie name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipie description'
+        )
+
+        self.assertEqual(str(recipie), recipie.title)
